609f65f4f220c4bfbb49b1e5321a5a66e0dbc1ea
Jakkoble <jakob.wassertheurer@eco-landeck.at>
2022-07-23 15:38:57 +0200

    Updated Tutorial Comments

--- src/main/kotlin/de/jakkoble/Fruits.kt
+++ src/main/kotlin/de/jakkoble/Fruits.kt
@@ -1,6 +1,6 @@
 package de.jakkoble
 
-// An object is used if there is just one object inside, in my example the Fruit List.
+// An Object is used instead of a class to hold just one object, in my example the Fruit List.
 // It's accessable to all classes.
 object Fruits {
     val list: MutableList<Fruit> = mutableListOf(
