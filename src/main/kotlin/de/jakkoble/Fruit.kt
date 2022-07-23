fa1e7345eee97b0cc39fd4ec9c022b175a2d4e3f
Jakkoble <jakob.wassertheurer@eco-landeck.at>
2022-07-23 15:36:37 +0200

    Updated Tutorial Comments

--- src/main/kotlin/de/jakkoble/Fruit.kt
+++ src/main/kotlin/de/jakkoble/Fruit.kt
@@ -1,9 +1,10 @@
 package de.jakkoble
 
-// A data class is used if the only reason of the class is to store data
+// A data class is used if the only reason of the class is to store data (In this Case the 'name', 'favorite' and 'path')
+// For further Information about Data Classes visit: https://kotlinlang.org/docs/data-classes.html
 data class Fruit(val name: String, val favorite: Boolean) {
 
-    // Accessable with the class, not Instance of it
+    // Accessable with the class, not Instance of it (Fruit.path => "/fruits")
     companion object {
         const val path = "/fruits"
     }
